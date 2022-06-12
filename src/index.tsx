fe334b1bc82947d39727483b8dd8bf18b1343da2
kaname <sonyapple2619@gmail.com>
2022-06-12 12:46:26 +0000

    ssr

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.hydrateRoot(document.getElementById('root')!, <App />)
+ReactDOM.hydrate(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
@@ -18,4 +18,4 @@ ReactDOM.hydrateRoot(document.getElementById('root')!, <App />)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    target: "node",
+    entry: {
+        server: "./server/index.tsx",
+        "public/client": "./src/index.tsx",
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                // exclude: /\..*test.*/,
+                use: {
+                    loader: "ts-loader",
+                    options: {
+                        compilerOptions: {
+                            strict: false,
+                            strictNullChecks: false,
+                        },
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            modules: [path.resolve("./src"), "node_modules"],
+            crypto: require.resolve("crypto-browserify"),
+            zlib: require.resolve("browserify-zlib"),
+            fs: require.resolve("fs"),
+            http: require.resolve("stream-http"),
+            assert: require.resolve("assert/"),
+        },
+        extensions: [".ts", ".js", ".tsx"],
+        plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+    },
+    externals: {
+        "node-fetch": "commonjs2 node-fetch",
+    },
+};
