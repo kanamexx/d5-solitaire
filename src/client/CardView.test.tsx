382d5b36d8417ab3cc4cb09c39538f548ad3e0e9
kaname <sonyapple2619@gmail.com>
2022-07-10 00:21:06 +0000

    setup UT environment for rendering

---
+++ src/client/CardView.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import Card from "../shared/domain/card/Card";
+import Rank from "../shared/domain/card/Rank";
+import Suit from "../shared/domain/card/Suit";
+import CardVeiw from "./CardView";
+
+describe("appearance", () => {
+  test("must have class card", () => {
+    const card = Card.of(Suit.CLUB, Rank.ACE, true);
+    const renderResult = render(<CardVeiw card={card} />);
+    expect(renderResult.container.getElementsByClassName("card").length).toBe(
+      1
+    );
+  });
+});
